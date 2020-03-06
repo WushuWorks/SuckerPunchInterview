f06b9ec44474516f53e99b79e5a855267177eae4
WushuWorks <52196708+WushuWorks@users.noreply.github.com>
2020-03-06 02:17:14 -0800

    Create memory structure and debugging print function

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Sucker Punch Interview Programming Problem
 
 This is my solution to the pre-screening problem for Sucker Punch Studios for a Gameplay Programming position.
+
+# Memory Structure
+
+Our memory coordinate structure is laid out like this
+
+> index				purpose
+>-------------------------------
+> 0-67				Freelist (index+1 == block number)
+> 68				Q Queue Count
+> 69 - 324			Q Queue Location
+> 325 - 349			Q Block 1
+> 350 - 374			Q Block 2
+>			...
+> 2000 - 2024		Q Block 68
+> 2025 - 2047		Unused
+
+Total Memory		 - 2048 Bytes
+Max Queues			 - 64
+Block Size			 - 25 Bytes
+Max Number of Blocks - 68
+
+We use constants and calculate boundaries with them so we can easily change the size and shape of our
+memory structure with a few simple changes instead of hard coding a structure into our solution
--- SuckerPunchInterview.cpp
+++ SuckerPunchInterview.cpp
@@ -1,7 +1,5 @@
 // SuckerPunchInterview.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-
-
 /*
 Requirements:
 - Type Q can be anything
@@ -29,19 +27,26 @@ Data Types, Structs, Constants, and Data
 */
 
 //Data Types
-typedef unsigned char ByteBlock;
+typedef unsigned int Bit;
 
 //Byte queue type - Q
 struct Q {
-    Q * head;
-    Q * tail;
-    unsigned int size;
-    ByteBlock* byteArray;
+    Bit alloc       : 1;
+    Bit head        : 7;
+    Bit headOffset  : 4;
+    Bit tail        : 7;
+    Bit tailOffset  : 4;
+    Bit budBlock    : 7; 
+
+    Bit empty       : 2;
 };
 
 // Constants
 const unsigned int MAX_QUEUES = 64;
 const unsigned int TOTAL_MEM_SIZE = 2048;
+const unsigned int Q_BLOCK_SIZE = 25; // Size in bytes
+const unsigned int MAX_Q_BLOCKS = 68; // Max number of blocks
+const unsigned int Q_QUEUE_SIZE = sizeof(Q);
 
 // Resources - provided by problem description
 unsigned char data[TOTAL_MEM_SIZE]; // Total available memory for storage from 0 - TOTAL_MEM_SIZE-1
@@ -65,8 +70,13 @@ unsigned char * GetStorage(){
 
 // Run first to setup memory
 void init_memory() {
-    //Init all memory to 0
-    memset(GetStorage(), 0, TOTAL_MEM_SIZE);
+    
+    //Mark freelist as completely free
+    memset(GetStorage(), '1', MAX_Q_BLOCKS);
+    //Mark Q_Queue count to 0
+    GetStorage()[MAX_Q_BLOCKS] = 0;
+    //Init everything else to null
+    memset(GetStorage() + MAX_Q_BLOCKS + 1, 0, TOTAL_MEM_SIZE - (MAX_Q_BLOCKS + 1));
 }
 
 // Creates a FIFO byte queue, returning a handle to it.
@@ -74,10 +84,10 @@ Q * create_queue() {
     Q* newQueue = NULL;
 
     // Allocate a queue and track how many are allocated
-    if (GetStorage()[1] < MAX_QUEUES) {
+    if (GetStorage()[MAX_Q_BLOCKS] < MAX_QUEUES) {
 
 
-        GetStorage()[1] += 1;
+        GetStorage()[MAX_Q_BLOCKS] += 1;
     }
 
     return newQueue;
@@ -98,7 +108,61 @@ unsigned char dequeue_byte(Q * q) {
 
 }
 
+// Prints the current state of memory for debugging
+void print_mem(unsigned char* memory) {
+    // This is the threshold for memory block storage starts
+    unsigned int q_block_mem_start = MAX_QUEUES * Q_QUEUE_SIZE + MAX_Q_BLOCKS + 1; // first index of storage - 325
+    unsigned int end_of_storage = MAX_Q_BLOCKS * Q_BLOCK_SIZE + q_block_mem_start - 1; // last index of storage - 2024
+    unsigned int items_per_row = 5;
+    unsigned int row_count = 0;
 
+    std::cout << "Memory Contents:\n";
+    for (unsigned int i = 0; i < TOTAL_MEM_SIZE; i++) {
+
+        if (i < MAX_Q_BLOCKS) { // Freelist scope
+            if (!i) {
+                printf("\nFreelist: Index - %d\n\n", i);
+            }
+            printf("[%c] ", memory[i]);
+        }
+        else if (i == MAX_Q_BLOCKS) { // Q Count
+            printf("\n\nQ Count: Index - %d\n\n", i);
+            printf("[%d]\n", memory[i]);
+        }
+        else if (i < q_block_mem_start) { // Q Queue location Scope
+            if (i == MAX_Q_BLOCKS+1) {
+                printf("\nQ Queue Location: Index - %d\n\n", i);
+                items_per_row = 8;
+                row_count = 0;
+            }
+            printf("[%c] ", memory[i]);
+        }
+        else if (i < end_of_storage + 1) { //Memory block scope
+            if (i == q_block_mem_start) {
+                printf("\nQ Block Memory: Index - %d\n\n", i);
+                items_per_row = Q_BLOCK_SIZE;
+                row_count = 0;
+            }
+            printf("[%c] ", memory[i]);
+        }
+        else { // Unused memory location
+            if (i == end_of_storage + 1) {
+                printf("\nUnused Memory: Index - %d\n\n", i);
+                items_per_row = 10;
+                row_count = 0;
+            }
+            printf("[%c] ", memory[i]);
+        }
+        row_count++;
+
+        if (!(row_count % items_per_row)) {
+            std::cout << std::endl;
+            row_count = 0;
+        }
+    }
+
+    std::cout << std::endl;
+}
 
 int main()
 {
@@ -107,6 +171,7 @@ int main()
     cout << "Hello SuckerPunch!\n";
 
     init_memory();
+    print_mem(GetStorage());
 
     /*
     Q* q0 = create_queue();
