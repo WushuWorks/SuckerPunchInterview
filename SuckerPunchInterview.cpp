469308f644c4f3cf4f25b0889f5acd14c94257b6
WushuWorks <52196708+WushuWorks@users.noreply.github.com>
2020-03-07 23:01:25 -0800

    Add logic to create queue in memory

--- SuckerPunchInterview.cpp
+++ SuckerPunchInterview.cpp
@@ -8,11 +8,9 @@ Requirements:
 - 64 queues at max
 - if we cannot satisfy a request due to lack of memory call provided function
 - if an illegal request is made use provided function
-
 Provided Functions:
  - void on_out_of_memory();
  - void on_illegal_operation();
-
 Notes:
     - Worst case performance is more important than average case performance for deletion and insertion
     - Spikes in queue size or number may occur
@@ -20,7 +18,9 @@ Notes:
     - 15 queues with 80~ bytes average
 */
 
-#include <iostream>
+#include <iostream> 
+#include <string.h> //memset
+#include <assert.h>
 
 /*
 Data Types, Structs, Constants, and Data
@@ -31,14 +31,14 @@ typedef unsigned int Bit;
 
 //Byte queue type - Q
 struct Q {
-    Bit alloc       : 1;
-    Bit head        : 7;
-    Bit headOffset  : 4;
-    Bit tail        : 7;
-    Bit tailOffset  : 4;
-    Bit budBlock    : 7; 
+    Bit alloc : 1;
+    Bit head : 7;
+    Bit headOffset : 4;
+    Bit tail : 7;
+    Bit tailOffset : 4;
+    Bit budBlock : 7;
 
-    Bit empty       : 2;
+    Bit empty : 2;
 };
 
 // Constants
@@ -50,61 +50,93 @@ const unsigned int Q_QUEUE_SIZE = sizeof(Q); // Size of our Q type queue
 
 // Resources - provided by problem description
 unsigned char data[TOTAL_MEM_SIZE]; // Total available memory for storage from 0 - TOTAL_MEM_SIZE-1
-/*
-Memory layout
-
-index       purpose
-------------------------------------------
-0           Freelist
-1           Number of allocated queues
-*/
 
 /*
 Helper functions
 */
 
-// Handler to avoid shadowing member of std namespace
-unsigned char * GetStorage(){
-    return data;
+// Given the Q Queues number 0-63, return the correct start location in memory
+unsigned int calculate_q_queue_index(unsigned int queue_number) {
+    return queue_number * Q_QUEUE_SIZE + MAX_Q_BLOCKS + 1;
+}
+
+// Given the Q_block number 0-67, reutrn the correct start location in memory
+unsigned int calculate_q_block_index(unsigned int block_number) {
+    return block_number * Q_QUEUE_SIZE + MAX_Q_BLOCKS + 1;
+}
+
+// Find the next free Q queue location in memory or return the maximum number of Queues
+// if none are available return TOTAL_MEM_SIZE
+unsigned int find_next_q_index() {
+    unsigned int return_index = TOTAL_MEM_SIZE;
+
+    for (int i = 0; i < MAX_QUEUES; i++) {
+        unsigned int current_index = calculate_q_queue_index(i);
+        Q* queue = reinterpret_cast<Q*>(&data[current_index]);
+        if (!queue->alloc) {
+            return_index = current_index;
+            break;
+        }
+    }
+
+    return return_index;
 }
 
 // Run first to setup memory
 void init_memory() {
-    
     //Mark freelist as completely free
-    memset(GetStorage(), '1', MAX_Q_BLOCKS);
+    memset(data, '1', MAX_Q_BLOCKS);
     //Mark Q_Queue count to 0
-    GetStorage()[MAX_Q_BLOCKS] = 0;
+    data[MAX_Q_BLOCKS] = 0;
     //Init everything else to null
-    memset(GetStorage() + MAX_Q_BLOCKS + 1, 0, TOTAL_MEM_SIZE - (MAX_Q_BLOCKS + 1));
+    memset(data + MAX_Q_BLOCKS + 1, 0, TOTAL_MEM_SIZE - (MAX_Q_BLOCKS + 1));
+}
+
+// Tests the correctnes of helper functions, return if 100% pass, crash otherwise
+void test_helper() {
+    //Note all tests are calculated for default settings
+    assert(calculate_q_queue_index(1) == 69);
+    assert(calculate_q_queue_index(2) == 73);
+    assert(calculate_q_queue_index(64) == 321);
+
+    assert(calculate_q_block_index(1) == 325);
+    assert(calculate_q_block_index(2) == 350);
+    assert(calculate_q_block_index(68) == 2000);
 }
 
 // Creates a FIFO byte queue, returning a handle to it.
-Q * create_queue() {
+Q* create_queue() {
     Q* newQueue = NULL;
 
     // Allocate a queue and track how many are allocated
-    if (GetStorage()[MAX_Q_BLOCKS] < MAX_QUEUES) {
+    int queue_ind = find_next_q_index();
+    if (queue_ind != TOTAL_MEM_SIZE) {
+        Q tempQueue = Q { 1U, 128U, 16U, 128U, 16U, 128U, 3U };
 
+        memcpy(&data[queue_ind], &tempQueue, sizeof(Q));
+        newQueue = &tempQueue;
 
-        GetStorage()[MAX_Q_BLOCKS] += 1;
+        data[MAX_Q_BLOCKS] += 1;
+    }
+    else {
+        assert(false); //call provided function
     }
 
     return newQueue;
 }
 
 // Destroy an earlier created byte queue.
-void destroy_queue(Q * q) {
+void destroy_queue(Q* q) {
 
 }
 
 // Adds a new byte to a queue.
-void enqueue_byte(Q * q, unsigned char b) {
+void enqueue_byte(Q* q, unsigned char b) {
 
 }
 
 // Pops the next byte off the FIFO queue
-unsigned char dequeue_byte(Q * q) {
+unsigned char dequeue_byte(Q* q) {
 
 }
 
@@ -113,7 +145,7 @@ void print_mem(unsigned char* memory) {
     // This is the threshold for memory block storage starts
     unsigned int q_block_mem_start = MAX_QUEUES * Q_QUEUE_SIZE + MAX_Q_BLOCKS + 1; // first index of storage - 325
     unsigned int end_of_storage = MAX_Q_BLOCKS * Q_BLOCK_SIZE + q_block_mem_start - 1; // last index of storage - 2024
-    unsigned int items_per_row = 5;
+    unsigned int items_per_row = 10;
     unsigned int row_count = 0;
 
     std::cout << "Memory Contents:\n";
@@ -130,7 +162,7 @@ void print_mem(unsigned char* memory) {
             printf("[%d]\n", memory[i]);
         }
         else if (i < q_block_mem_start) { // Q Queue location Scope
-            if (i == MAX_Q_BLOCKS+1) {
+            if (i == MAX_Q_BLOCKS + 1) {
                 printf("\nQ Queue Location: Index - %d\n\n", i);
                 items_per_row = 8;
                 row_count = 0;
@@ -148,7 +180,7 @@ void print_mem(unsigned char* memory) {
         else { // Unused memory location
             if (i == end_of_storage + 1) {
                 printf("\nUnused Memory: Index - %d\n\n", i);
-                items_per_row = 10;
+                items_per_row = 25;
                 row_count = 0;
             }
             printf("[%c] ", memory[i]);
@@ -166,15 +198,16 @@ void print_mem(unsigned char* memory) {
 
 int main()
 {
-    using namespace std;
 
-    cout << "Hello SuckerPunch!\n";
+    std::cout << "Hello SuckerPunch!\n";
 
+    //Run test suite on helper functions, crash on failure
+    test_helper();
     init_memory();
-    print_mem(GetStorage());
-
-    /*
+    
     Q* q0 = create_queue();
+    /*
+    
     enqueue_byte(q0, 0);
     enqueue_byte(q0, 1);
     Q* q1 = create_queue();
@@ -188,10 +221,12 @@ int main()
     printf("%d", dequeue_byte(q0));
     printf("%d", dequeue_byte(q0));
     destroy_queue(q0);
-
     printf("%d", dequeue_byte(q1));
     printf("%d", dequeue_byte(q1));
     printf("%d\n", dequeue_byte(q1));
     destroy_queue(q1);
     */
+    
+    //Examine memory
+    print_mem(data);
 }
